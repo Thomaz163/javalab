fec067e528b5267f9d14f635dc8cc12735449cb6
Thomas Mathew <146074540+Thomaz163@users.noreply.github.com>
2023-10-31 15:42:41 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class reversestring {
+	public static void main(String[] arg) {
+		String string;
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter the string");
+		string=sc.nextLine();
+		System.out.println("The Reverse of a string " + string + "is");
+		for(int j=string.length();j>0;--j) {
+			System.out.print(string.charAt(j-1));
+		}
+	}
+	}
